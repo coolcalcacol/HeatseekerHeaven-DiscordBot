4fa2f9e03b4a7a87cc887f0ea96b8d66ac731412
CTN <info@ctn-originals.com>
2022-07-27 20:07:37 +0200

    Improved InteractionCreate log message

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -9,25 +9,32 @@ module.exports = {
 				interaction.customId : 
 				interaction.commandName
 			;
-			const options = interaction.options;
-			const input = interaction.options._hoistedOptions;
-			const subcommand = options._subcommand ? 
-				`[style=bold][style=underscore][fg=blue]${options._subcommand}[/>]` : ''
-			;
-			var commandOptions = '';
-			
-			for (let i = 0; i < input.length; i++) {
-				const option = input[i];
-				if (option.value) {
-					commandOptions += `[fg=blue]${option.name}[/>]:[fg=cyan]${option.value}[/>] `
-				}
-			}
-			cConsole.log(
-				'[fg=cyan]' + interaction.user.tag + 
+
+			var logMessage = '[fg=cyan]' + interaction.user.tag + 
 				'[/>] in [style=inverse][fg=black]#' + interaction.channel.name + 
 				'[/>] triggered an interaction.\n' +
-				'[fg=yellow]/[fg=green]' + commandName + '[/>] ' + 
-				subcommand + '\n' + commandOptions + '\n', 
+				'[fg=yellow]/[fg=green]' + commandName + '[/>]'
+			;
+			const options = interaction.options;
+			if (options) {
+				const input = interaction.options._hoistedOptions;
+				const subcommand = options._subcommand ? 
+				` [style=bold][style=underscore][fg=blue]${options._subcommand}[/>]` : ''
+				;
+				
+				var commandOptions = '';
+				for (let i = 0; i < input.length; i++) {
+					const option = input[i];
+					if (option.value) {
+						commandOptions += `[fg=blue]${option.name}[/>]:[fg=cyan]${option.value}[/>] `
+					}
+				}
+				logMessage += subcommand + '\n';
+				logMessage = commandOptions != '' ? logMessage + commandOptions + '\n' : logMessage;
+			}
+
+			
+			cConsole.log(logMessage, 
 				{autoColorize: false}
 			);
 		}
