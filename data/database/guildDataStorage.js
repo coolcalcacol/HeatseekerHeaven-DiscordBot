be43fcd13b9bdf8a55d85eb614f887d62f922f9c
CTN <info@ctn-originals.com>
2022-12-05 14:04:27 +0100

    Update guildDataStorage.js
    tried to store timers in the database but the callbacks did not go with it so this is now redundant

--- data/database/guildDataStorage.js
+++ data/database/guildDataStorage.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const GuildDataSchema = new mongoose.Schema({
     _id: String,
+    // activeTimers: {type: Object, default: {}},
 });
 
 module.exports = mongoose.model('GuildDatabase', GuildDataSchema);
