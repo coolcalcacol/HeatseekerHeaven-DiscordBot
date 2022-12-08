03a0df99781e0e025d8f848401a389241df6b2bd
CTN <info@ctn-originals.com>
2022-12-08 11:00:10 +0100

    Update generalData.js

--- data/generalData.js
+++ data/generalData.js
@@ -6,17 +6,17 @@ const { Client } = require('discord.js');
 */
 module.exports = {
     client: null,
-    debugMode: false,
-    releasedVersion: true,
+    debugMode: true,
+    releasedVersion: false,
     botConfig: {
         defaultGuildId: '349360638088314881',
         botSetupGuildId: '811667577985302528',
     },
     generalQueueSettings: {
-        pauseQueue: true,
+        pauseQueue: false,
     },
     debugOptions: {
-        createGameOnStart: false,
+        createGameOnStart: true,
         gameOnStartLobby: 'threes'
     },
     logOptions : {
@@ -37,10 +37,8 @@ module.exports = {
         upTime: null,
     },
     userWhitelist: [
-        // '267442458638417921', // NoLimitGoten [lEFT THE SERVER]
         // '479936093047750659', // 888% [Bypass]
         // '382279435828723716', // FinnayBusiness [Bypass]
-        // '614257446654967813', // orangecod
         '280432147695665163', // Joshh
         '599339755662082057', // Darn
         '688819598686289952', // Lxyer
@@ -54,7 +52,6 @@ module.exports = {
         '492497679570436117', // CSmith_Games
         '95630080893521920',  // kaelan
         '568449733228756993', // Bramble
-        // '178625919559270409', // ncj
         '198802539783651328', // tavz
         '201039454930993152', // Marshmallow
         '510829824483524638', // coco_ice
@@ -64,14 +61,14 @@ module.exports = {
         '653453312271581205', // RedRockGaming69
         '862875902621777951', // Zerkoxito
         '467786597878857740', // DarthShadoww
-        // '510636937489416196', // Nate
         '700707631915991051', // Zebby
         '646927153614553122', // Mxnny
-        // '665951998713004051', // Pew
         '793278749138747423', // PogChamp
         '509407463565557761', // Quilln
         '434732958360797204', // TOG
         '476834059431968772', // Hunter
         '393061621393784833', // Egol
+        // '510636937489416196', // Nate [left server]
+        // '178625919559270409', // ncj [left server]
     ]
 }
